18a39f907f2b14480602fff36ec39eaf6cc0e2de
Anderson Lopes <zeander21@outlook.com.br>
2024-08-19 20:17:37 -0300

    We like a little fooling around

--- src/components/ui/Button/button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "./button";
-
-export default {
-  component: Button,
-  title: "Button",
-  tags: ["ui"],
-};
-
-export const Default = {};
--- src/components/ui/Button/button.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { Button } from "./button";
-import { test } from "vitest";
-
-test("Button component renders without crashing", () => {
-  render(<Button />);
-});
